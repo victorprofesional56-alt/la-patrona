18dda16b9ae72ced75bdb750d0653b2d7a01a172
Biktoor56 <victorprofesional56@gmail.com>
2026-08-16 21:08:47 -0600

    Convertir app a PWA (offline + instalable)

---
+++ sw.js
@@ -0,0 +1,51 @@
+"use strict";
+
+var CACHE = "la-patrona-v1";
+var CORE = [
+  "./",
+  "./index.html",
+  "./logo-hamster.svg",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png"
+];
+
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(function (c) { return c.addAll(CORE); })
+      .then(function () { return self.skipWaiting(); })
+  );
+});
+
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys()
+      .then(function (keys) {
+        return Promise.all(
+          keys.filter(function (k) { return k !== CACHE; })
+              .map(function (k) { return caches.delete(k); })
+        );
+      })
+      .then(function () { return self.clients.claim(); })
+  );
+});
+
+self.addEventListener("fetch", function (e) {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(function (hit) {
+      if (hit) return hit;
+      return fetch(e.request).then(function (res) {
+        if (res && res.ok) {
+          var copy = res.clone();
+          caches.open(CACHE).then(function (c) { c.put(e.request, copy); });
+        }
+        return res;
+      }).catch(function () {
+        return caches.match("./");
+      });
+    })
+  );
+});
